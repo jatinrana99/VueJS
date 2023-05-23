cc40bfb5a12e9ffe6658c1a5c38c4699c1f5ac70
Jatin rana <engr.jatinrana@gmail.com>
2023-05-23 15:03:51 +0530

    Created One another component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Test/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="testing">
+        {{name}} Testing
+        <br/>
+        <br/>
+
+        <input type="text" v-model="name" />
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'TestJatin',
+    data:  () => {
+        return{
+            name:  "Jatin"
+        }
+    }
+};
+</script>
+
+<style>
+#testing {height: 45px ;
+    width : 102px}
+</style>
