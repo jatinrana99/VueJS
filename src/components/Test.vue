738e2da7f8608889fd48b87e05d7b7cd50278d71
Jatin rana <engr.jatinrana@gmail.com>
2023-05-23 17:46:33 +0530

    Pass the data from script to template

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,10 +1,15 @@
 <template>
     <div id="testing">
-        {{name}} Testing
+        Hello {{name}}
         <br/>
         <br/>
 
         <input type="text" v-model="name" />
+        <button >Clear</button>
+        <button>Captial</button>
+
+
+        <h4>Email {{Email}}</h4>
     </div>
 </template>
 
@@ -12,15 +17,32 @@
 <script>
 export default {
     name: 'TestJatin',
+
+
     data:  () => {
         return{
-            name:  "Jatin"
+            name:  "",
+            Email : `jatin@123`
         }
+    },
+
+
+
+    // allData()   we can not do like this we have to pass data or states in data function only and it can be only one ... 
+    //     {
+    //     return {
+    //     }
+    //  }
+
+
     }
-};
+    // age: 45 can't define like this (for passing the data in a template)
+
+    // for that we have to make a function
 </script>
 
-<style>
-#testing {height: 45px ;
-    width : 102px}
+<style scoped>
+ /* #testing {
+    color: brown;
+ } */
 </style>
