d92f0150cc79bc515b7b01035ece90f20f0f3402
Jatin rana <engr.jatinrana@gmail.com>
2023-06-01 15:17:15 +0530

    PageNotFound Added

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Error 404</h1>
+    <div>Page not found</div>
+</template>
+
+<script>
+export default {
+    name:`PageNotFound`
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {createWebHistory , createRouter} from 'vue-router'
 import Home from './components/Home.vue';
 import Login from './components/Login.vue';
 import Profile from './components/Profile.vue';
+import PageNotFound from './components/PageNotFound.vue';
 
 const routes =[
 {
@@ -18,7 +19,12 @@ const routes =[
     name:`Profile`,
     path:`/profile/:name`,
     component:Profile
-}
+},
+ {
+    name:`NotFound`,
+    path:'/:pathMatch(.*)*',
+    component:PageNotFound
+ }
 ];
 
 const router = createRouter({
